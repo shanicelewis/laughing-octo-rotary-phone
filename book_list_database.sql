103ec9c7bb16c84555098f66d74821b3f4d272f1
shanlew <112531829+shanicelewis@users.noreply.github.com>
2022-08-31 19:50:26 -0500

    Add files via upload

---
+++ book_list_database.sql
@@ -0,0 +1,4 @@
+CREATE TABLE favoritebooks (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+INSERT INTO favoritebooks VALUES (1, "How to Kill a Mockingbird", 4);
+INSERT INTO favoritebooks VALUES (2, "Five Love Languages", 5);
+INSERT INTO favoritebooks VALUES (3, "The Notebook", 5);
